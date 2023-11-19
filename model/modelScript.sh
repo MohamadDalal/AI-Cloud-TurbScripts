2a481547526bf6fd9088f7108c282e2b5f0dc1c4
mdalal20 <mdalal20@student.aau.dk@ai-fe02>
2023-11-19 19:57:16 +0100

    Successfuly ran first training on AI-Cloud

--- model/modelScript.sh
+++ model/modelScript.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-srun --gres=gpu:1 --time=1-00 singularity exec --nv ~/pytorch_23.09-py3.sif python main.py > log.txt
+srun --time=1-00 singularity exec --nv ~/pytorch_23.09-py3.sif python main.py
