e0b4683925be5f3b119c2c7b1623323cb9d8faa2
mdalal20 <mdalal20@student.aau.dk@ai-fe02>
2023-11-26 21:02:22 +0100

    Made scripts to run tests with slurm

--- model/testTime.py
+++ model/testTime.py
@@ -1,5 +1,5 @@
 from time import perf_counter
-from data import get_test_set
+from data import get_training_set, get_test_set
 from torch.utils.data import DataLoader
 import torch
 
@@ -10,13 +10,11 @@ else:
     device = torch.device("cpu")
 
 for num_workers in range(4, 33, 4):  
-    dataset = get_test_set()
+    dataset = get_training_set()
     #print(num_workers)
-    test_data_loader = DataLoader(dataset=dataset, num_workers=4, batch_size=32 , shuffle=False)
+    test_data_loader = DataLoader(dataset=dataset, num_workers=4, batch_size=128 , shuffle=False)
     start = perf_counter()
-    for epoch in range(1, 3):
-        for batch in test_data_loader:
-            input, target = batch[0].to(device), batch[1].to(device)
-        #print(epoch)
+    for batch in test_data_loader:
+        input, target = batch[0].to(device), batch[1].to(device)
     end = perf_counter()
     print("Finish with:{} second, num_workers={}".format(end - start, num_workers))
