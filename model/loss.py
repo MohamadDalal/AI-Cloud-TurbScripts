6f420cae346ad5d9641f38a067de3a8c36bff48c
Alexander <alexfoergaard@gmail.com>
2023-11-29 14:01:37 +0100

    Divergence loss implemented.

--- model/loss.py
+++ model/loss.py
@@ -1,6 +1,30 @@
 import numpy as np
+import torch
+import torch.nn as nn
 
-class ns_loss:
-    def __init__():
-        ground_truth = np.load("placeholder")
-        input = np.load("Placeholer")
+
+class div_loss(nn.Module):
+    def __init__(self):
+        super(div_loss, self).__init__()
+
+
+    def calc_div(self, data):
+        x_grad_x, x_grad_y = np.gradient(data[:, :, 0])
+        y_grad_x, y_grad_y = np.gradient(data[:, :, 1])
+        z_grad_x, z_grad_y = np.gradient(data[:, :, 2])
+
+        divergence_x = x_grad_x + x_grad_y
+        divergence_y = y_grad_x + y_grad_y
+        divergence_z = z_grad_x + z_grad_y
+
+        tot_div_x = np.sum(divergence_x)
+        tot_div_y = np.sum(divergence_y)
+        tot_div_z = np.sum(divergence_z)
+
+        return (tot_div_x + tot_div_y + tot_div_z)/3
+        
+
+    def forward(self, predictions, targets):
+        pred_div = self.calc_div(predictions)
+        target_div = self.calc_div(targets)
+        return abs(pred_div - target_div)
