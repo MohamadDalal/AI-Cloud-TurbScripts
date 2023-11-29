0bd0c1b539d0f546b7e5b83fe3d3f9d7e3f2c092
mdalal20 <mdalal20@student.aau.dk@ai-fe02>
2023-11-29 14:51:36 +0100

    Generated less downsampled data, and split into validation and test

--- model/data/all_data/moveFromTrain.py
+++ model/data/all_data/moveFromTrain.py
@@ -18,21 +18,21 @@ for x in validation_list:
         if not os.path.isfile(f"{input_dir}/{x}{i}.npy"):
             print(f"File {input_dir}/{x}{i}.npy does not exist")
             continue
-        elif not os.path.isfile(f"{input_dir2}/{x}{i}.npy"):
-            print(f"File {input_dir2}/{x}{i}.npy does not exist")
-            continue
+#        elif not os.path.isfile(f"{input_dir2}/{x}{i}.npy"):
+#            print(f"File {input_dir2}/{x}{i}.npy does not exist")
+#            continue
         else:
             os.popen(f"mv {input_dir}/{x}{i}.npy {validation_dir}/{x}{i}.npy")
-            os.popen(f"mv {input_dir2}/{x}{i}.npy {validation_dir2}/{x}{i}.npy")
+#            os.popen(f"mv {input_dir2}/{x}{i}.npy {validation_dir2}/{x}{i}.npy")
 
 for x in test_list:
     for i in range(10):
         if not os.path.isfile(f"{input_dir}/{x}{i}.npy"):
             print(f"File {input_dir}/{x}{i}.npy does not exist")
             continue
-        elif not os.path.isfile(f"{input_dir2}/{x}{i}.npy"):
-            print(f"File {input_dir2}/{x}{i}.npy does not exist")
-            continue
+#        elif not os.path.isfile(f"{input_dir2}/{x}{i}.npy"):
+#            print(f"File {input_dir2}/{x}{i}.npy does not exist")
+#            continue
         else:
             os.popen(f"mv {input_dir}/{x}{i}.npy {test_dir}/{x}{i}.npy")
-            os.popen(f"mv {input_dir2}/{x}{i}.npy {test_dir2}/{x}{i}.npy")
+#            os.popen(f"mv {input_dir2}/{x}{i}.npy {test_dir2}/{x}{i}.npy")
