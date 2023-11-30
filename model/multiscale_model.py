8dd99fdca805f762c7c91c3479cc9d1905581a26
mputak <mputak98@gmail.com>
2023-11-30 15:57:17 +0100

    multiscale_model_initial

--- model/multiscale_model.py
+++ model/multiscale_model.py
@@ -23,48 +23,111 @@ class Net(nn.Module):
         self.upscale_factor = upscale_factor
         self.relu = nn.ReLU()
 
-       # first layer
+
+        # skip-connections
+        # 1
+        self.pool1 = nn.MaxPool2d(8, stride=8)
+        self.convs1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.convs2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.upsample1 = nn.Upsample(scale_factor=2, mode='nearest')
+
+        # 2
+        self.pool2 = nn.MaxPool2d(4, stride=4)
+        self.convs3 = nn.Conv2d(35, 32, kernel_size=3, padding=1)
+        self.convs4 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.upsample2 = nn.Upsample(scale_factor=2, mode='nearest')
+
+        # 3
+        self.pool3 = nn.MaxPool2d(2, stride=2)
+        self.convs5 = nn.Conv2d(35, 32, kernel_size=3, padding=1)
+        self.convs6 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.upsample3 = nn.Upsample(scale_factor=2, mode='nearest')
+        #pix-shuff
+        # self.convs7 = nn.Conv2d(32, 3 * self.upscale_factor ** 2, (3,3), (1,1), (1,1))
+        # self.pixel_shuffle = nn.PixelShuffle(self.upscale_factor)
+
+        # 4
+        self.convs8 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.convs9 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+
+        # first layer
         self.conv1 = nn.Conv2d(3, 16, kernel_size=5, padding=2) #padding = (kernelsize-1)/2 to ensure same resolution as input
-        self.conv2 = nn.Conv2d(16, 16, kernel_size=9, padding=4)
-        self.conv3 = nn.Conv2d(16, 16, kernel_size=13, padding=6)
+        self.conv2 = nn.Conv2d(16, 8, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(8, 8, kernel_size=5, padding=2)
         
 
         # second layer
-        self.conv4 = nn.Conv2d(48, 8, kernel_size=5, padding=2)
-        #self.conv4 = nn.Conv2d(48, 24, kernel_size=5, padding=2)  # Assuming x2_1, x2_2, and x2_3 each have 8 channels
-        self.conv5 = nn.Conv2d(8, 8, kernel_size=9, padding=4)
-        self.conv6 = nn.Conv2d(8, 8, kernel_size=13, padding=6)
+        self.conv4 = nn.Conv2d(3, 16, kernel_size=9, padding=4)
+        self.conv5 = nn.Conv2d(16, 8, kernel_size=9, padding=4)
+        self.conv6 = nn.Conv2d(8, 8, kernel_size=9, padding=4)
 
+        # third layer
+        self.conv7 = nn.Conv2d(3, 16, kernel_size=13, padding=6)
+        self.conv8 = nn.Conv2d(16, 8, kernel_size=13, padding=6)
+        self.conv9 = nn.Conv2d(8, 8, kernel_size=13, padding=6)
 
-        #sub-pixel convolutional layer
-        self.conv7 = nn.Conv2d(24, 3 * self.upscale_factor ** 2, (3, 3), (1, 1), (1, 1))
+        # fourth layer
+        self.conv10 = nn.Conv2d(27, 8, kernel_size=7, padding=3)
+        self.conv11 = nn.Conv2d(8, 3, kernel_size=5, padding=2)
+
+        # final layer/sub-pixel convolutional layer
+        self.conv12 = nn.Conv2d(35, 3 * self.upscale_factor ** 2, (3, 3), (1, 1), (1, 1) )
         self.pixel_shuffle = nn.PixelShuffle(self.upscale_factor)
-
-        
 
 
         self._initialize_weights()
 
     def forward(self, x):
 
-        #first layer
-        x1_1 = self.relu(self.conv1(x))
+        input_ = x
+        down1 = self.pool1(input_)
+        x1s = self.relu(self.convs1(down1))
+        x1s = self.relu(self.convs2(x1s))
+        x1s = self.upsample1(x1s)
+
+        down2 = self.pool2(input_)
+        x2s = torch.cat((x1s, down2), dim=1)
+        x2s = self.relu(self.convs3(x2s))
+        x2s = self.relu(self.convs4(x2s))
+        x2s = self.upsample2(x2s)
+
+        down3 = self.pool3(input_)
+        x3s = torch.cat((x2s, down3), dim=1)
+        x3s = self.relu(self.convs5(x3s))
+        x3s = self.relu(self.convs6(x3s))
+        x3s = self.upsample3(x3s)
+
+        x4s = torch.cat((x3s, input_), dim=1)
+        x4s = self.relu(self.convs8(x4s))
+        x4s = self.relu(self.convs9(x4s))
+
+        # first layer
+        x1_1 = self.relu(self.conv1(input_))
         x1_2 = self.relu(self.conv2(x1_1))
         x1_3 = self.relu(self.conv3(x1_2))
-        x1 = torch.cat((x1_1, x1_2, x1_3), dim=1)
 
-        #second layer
-        x2_1 = self.relu(self.conv4(x1))
+        # second layer
+        x2_1 = self.relu(self.conv4(input_))
         x2_2 = self.relu(self.conv5(x2_1))
         x2_3 = self.relu(self.conv6(x2_2))
-        x2 = torch.cat((x2_1, x2_2, x2_3), dim=1)
 
-        #pixel shuffle
-        x3 = self.pixel_shuffle(self.conv7(x2))
+        # third layer
+        x3_1 = self.relu(self.conv7(x))
+        x3_2 = self.relu(self.conv8(x3_1))
+        x3_3 = self.relu(self.conv9(x3_2))
+
+        # fourth layer
+        x4_1 = torch.cat((x1_3, x2_3, x3_3, input_), dim=1)
+        x4_2 = self.relu(self.conv10(x4_1))
+        x4_3 = self.relu(self.conv11(x4_2))
+
+        # pixel shuffle
+        x_final = torch.cat((x4s, x4_3), dim=1) 
+        x_final = self.pixel_shuffle(self.conv12(x_final))
+
+        return x_final
     
-
-        return x3
-
+    
     def _initialize_weights(self):
         init.orthogonal_(self.conv1.weight, init.calculate_gain('relu'))
         init.orthogonal_(self.conv2.weight, init.calculate_gain('relu'))
